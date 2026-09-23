3c4b7010bda8ae841fe75e881ab271852ff4c2bd
migueloliveira08-a11y <miguel.oliveira08@escola.pr.gov.br>
2026-09-23 08:36:29 -0300

    atulizei o script

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-const botao = document.querySelector("#botaoMensgemm");
+const botoesCurtir = document.querySelectorAll(".curtir");
+botoesCurtir.forEach(function(botaoCurtir){
+let curtiu = false;
+botaoCurtir.addEventListener("click, curtir");
+function curtir(){
+const contador = botaoCurtir.querySelector("span");
+if (curtir === false){
+contador.textContent++;
+}else{
+contador.textContent--;
+curtir = false;
 
-const mensagem = document.querySelector("#mensagemInterativa")
+}
+
+}  
+  
+});
