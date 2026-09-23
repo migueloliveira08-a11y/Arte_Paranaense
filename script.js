9531e0c9a81a876149399dc4e1761bef7a16ffdc
migueloliveira08-a11y <miguel.oliveira08@escola.pr.gov.br>
2026-09-23 08:54:08 -0300

    atualizei o script

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const botoesCurtir = document.querySelectorAll(".curtir");
 botoesCurtir.forEach(function(botaoCurtir){
 let curtiu = false;
-botaoCurtir.addEventListener("click, curtir");
+botaoCurtir.addEventListener("click", curtir);
 function curtir(){
 const contador = botaoCurtir.querySelector("span");
-if (curtir === false){
+if (curtiu === false){
 contador.textContent++;
+curtiu = true;
 }else{
 contador.textContent--;
-curtir = false;
+curtiu = false;
 
 }
 
